d56bf4bd1e4fce30279ddeee6163157097f17c4f
buzbird <humroom028@gmail.com>
2024-01-20 22:59:00 +0300

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     experimental: {
 		serverActions: {
 			allowedForwardedHosts: ['localhost'],
-			allowedOrigins: ['http://5.35.93.157','http://127.0.0.1',"website.com", "localhost:"]
+			allowedOrigins: ['http://5.35.93.157','http://127.0.0.1',"website.com", "localhost"]
 		},
 	}
 }
