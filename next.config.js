95d03db2c72b7cc46bbe15899c094f8dc00c49ff
buzbird <humroom028@gmail.com>
2024-01-22 16:53:01 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-		serverActions: {
-			allowedForwardedHosts: ['localhost'],
-			allowedOrigins: ['http://5.35.93.157','http://127.0.0.1',"website.com", "localhost"]
-		},
-	}
+        serverActions: {
+            allowedForwardedHosts: ['localhost'],
+            allowedOrigins: ['http://localhost']
+        },
+    }
 }
 
 module.exports = nextConfig
