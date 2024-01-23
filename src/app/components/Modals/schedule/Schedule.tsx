ecf54a731670b1a681d92f3bf9fa5850bd43c49c
buzbird <humroom028@gmail.com>
2024-01-23 13:53:06 +0300

    c

--- src/app/components/Modals/schedule/Schedule.tsx
+++ src/app/components/Modals/schedule/Schedule.tsx
@@ -103,6 +103,7 @@ const ScheduleTable = (data:any) => {
   }
   const createDateLesson2= async() =>{
     try {
+      console.log("data")
       console.log(group2,groupnmass.get(group2),lesson)
       const lesson2 = await fetch("/api/jurnal/getlessonid2/",{
         method:'POST',
