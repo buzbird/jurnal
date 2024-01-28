046951a2b860825fe949a9513ba6f1f4797256f4
buzbird <humroom028@gmail.com>
2024-01-28 12:53:58 +0300

    d

--- src/app/components/TheHeader.tsx
+++ src/app/components/TheHeader.tsx
@@ -2,19 +2,78 @@ import { getServerSession } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { authOptions } from '../api/auth/[...nextauth]/route';
-import Header2 from './Header';
 
 
+const getHeader = async(session:any) =>{
+    const header = await fetch(process.env.API +"/api/header",{
+        method:'POST',
+        body: JSON.stringify({email: session?.user?.email}),
+    })
+    const data = await header.json()
+    return data
+}
 
 export default async function TheHeader(){
     const session = await getServerSession(authOptions)
-    const log = session ==undefined
+    const log = session==undefined
+    const data = await getHeader(session)
+    console.log(log)
+    console.log(data)
     return (
             <>
             <header>
-            <div className='fc'><Link href="/">Home</Link></div>
-            <Header2 data={session}/>
-            <div><Link href="/schedule/student">расписание</Link></div>
+        <div className='fc'><Link href="/">Home</Link></div>
+
+        {data.map((data:any)=>{
+            return(
+                <>
+                {data.permission.map((permission:any)=>{
+                if (permission.permission_id == 1){
+                    return (<><div className='fc'><Link href="/admin/">Администратор</Link></div></>)
+                }
+            })}
+                </>
+            );
+            
+        })}
+        {data.map((data:any)=>{
+            return(
+            <>
+            {data.permission.map((permission:any)=>{
+                if (permission.permission_id == 3){
+                    <div className='fc'><Link href="/teacher/">Куратор</Link></div>
+                }
+            })} 
+            </>
+            );
+        })}
+        {data.map((data:any)=>{
+            return(<>
+            {data.permission.map((permission:any,index:any)=>{
+                if (permission.permission_id == 2){
+                    return(<div key={index} ><Link href="/schedule/">Составитель расписания</Link></div>);
+                }
+            })}
+            </>
+            );
+        })}
+        {data.map((data:any)=>{
+            return(<>
+            {data.permission.map((permission:any)=>{
+                 if (permission.permission_id == 4){<div ><Link href="/teacher/">Преподаватель</Link></div>}
+            })}</>)
+        })}
+        {data.map((data:any)=>{
+            return(<>
+            {data.permission.map((permission:any,index:any)=>{
+                if (permission.permission_id == 5){
+                    return (<div key={index}><Link href="/Admin/">студентов</Link></div>);
+            }
+        })}
+            </>
+            )
+        })}
+                <div><Link href="/schedule/student">расписание</Link></div>
         {log ? (<div ><Link href="/auth/signin">войти</Link></div>):(<div><Link href="/auth/signout">выйти</Link></div>)}
     </header>
             </>
