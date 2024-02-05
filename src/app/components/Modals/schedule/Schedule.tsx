a428fbcf3ea23dc768e76869266f22991444a634
buzbird <humroom028@gmail.com>
2024-02-06 02:37:58 +0300

    в

--- src/app/components/Modals/schedule/Schedule.tsx
+++ src/app/components/Modals/schedule/Schedule.tsx
@@ -25,7 +25,8 @@ const ScheduleTable = (data:any) => {
   const [lesson_number, setlesson_number] = useState(0);
   
   const [showModalgroup, setshowModalgroup] = useState(false);
-
+  const [showModaldate, setshowModaldate] = useState(false);
+  const [date2, setDate2] = useState(new Date());
   const [lesson,setLesson] = useState("");
   const [cabinet,setCabinet] = useState("");
   // запрос кабинетов
@@ -110,7 +111,27 @@ const ScheduleTable = (data:any) => {
       })
       
       setshowModalgroup(false);
-      setShowModal(true);
+    }catch(err){
+        console.log(err)
+    }
+  }
+  const createDateLesson3= async() =>{
+    try {
+      let group_id = await groupmass.get(group)
+      data = await fetch("/api/student/jurnal/",{
+        method:'POST',
+        body: JSON.stringify({date:new Date(date),group_id:group_id}),
+      })
+      console.log(group_id)
+      let lessons2 = await data.json()
+      group_id = await groupmass.get(group2)
+      console.log(group_id)
+      await fetch("/api/jurnal/createdateoflessonmany/", {
+        method:'POST',
+        body: JSON.stringify({lesson:lessons2,date:new Date(date2),group_id: group_id}),
+      })
+      
+      setshowModalgroup(false);
     }catch(err){
         console.log(err)
     }
@@ -516,8 +537,53 @@ const ScheduleTable = (data:any) => {
           </div>
         </>
       ) : null}
+{showModaldate ? (
+        <>
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-1 z-101 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h3 className="text-3xl font=semibold">выберите группу</h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                <div>
+                <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
+                <input type="date" onChange={(e) =>{setDate2(new Date(e.target.value))}}/>
+                </form>
+                </div>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setshowModalgroup(false)}
+                  >
+                    закрыть
+                  </button>
+                  <button
+                    className="text-white bg-yellow-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => createDateLesson3()}
+                  >
+                    добавить предметы
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+
     </>
   );
+
+
 };
 
 const ScheduleModelperTeacher = ({date}:any) => {
