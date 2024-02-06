75777bebe328e238b45d0d3020b7e1db8e539d5f
buzbird <humroom028@gmail.com>
2024-02-06 17:46:06 +0300

    d

--- src/app/teacher/page.tsx
+++ src/app/teacher/page.tsx
@@ -5,18 +5,14 @@ import { GetAllStatemnt,getUser,getTeacher } from '@/db/fetch'
 import { authOptions } from '../api/auth/[...nextauth]/route';
 const getLesson = async(session:any) =>{
   const user = await getUser(session.user?.email)
-  console.log(user)
   const teacher = await getTeacher(user?.id)
-  console.log(teacher)
   const data  = await GetAllStatemnt(teacher?.teacher_id,undefined,undefined)
   return data
  
 }
 const Jurnal = async() => {
   const session = await getServerSession(authOptions)
-  console.log(session)
   const data = await getLesson(session)
-  console.log(data)
       return (
         <JurnalForm lesson={data}/>
       )
