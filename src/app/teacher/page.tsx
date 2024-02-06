e198fb769228a5a10a12419b6760a204fe92b9de
buzbird <humroom028@gmail.com>
2024-02-06 17:35:25 +0300

    d

--- src/app/teacher/page.tsx
+++ src/app/teacher/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { getServerSession } from 'next-auth';
 import JurnalForm from '../components/Modals/jurnal/Jurnal'
 import { GetAllStatemnt } from '@/db/fetch'
+import { authOptions } from '../api/auth/[...nextauth]/route';
 const getLesson = async() =>{
   const data  = await GetAllStatemnt(1,undefined,undefined)
   return data
  
 }
 const Jurnal = async() => {
+  const session = await getServerSession(authOptions)
+  console.log(session)
   // const data = await getLesson()
       return (
         <> 
